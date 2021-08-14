542bdda1a554130ad205d954f1b43e7a1e655186
Unknown <tomatoesneverlie@gmail.com>
2021-08-13 20:52:03 -0400

    work towards shift layer

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -1,19 +1,27 @@
 #include "stag42.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+	//base
 	KEYMAP(
-		KC_ESC, KC_LCTL, KC_LGUI, KC_LALT, KC_DEL, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, 
-		MO(1), KC_A, KC_S, KC_D, KC_F, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, MO(1), KC_RSFT, 
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_SPC),
-
+		KC_ESC,  KC_LCTL,    KC_LGUI,  KC_LALT,  KC_DEL,  KC_T,  KC_Y,     KC_U,  KC_I,     KC_O,    KC_P,     KC_BSPC,
+		KC_TAB,  KC_Q,       KC_W,     KC_E,     KC_R,    KC_G,  KC_H,     KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_ENT,
+		MO(2),   KC_A,       KC_S,     KC_D,     KC_F,    KC_B,  KC_N,     KC_M,  KC_COMM,  KC_DOT,  MO(2),    LM(1,     MOD_RSFT),
+		LM(1,    MOD_LSFT),  KC_Z,     KC_X,     KC_C,    KC_V,  KC_SPC),
+ 	//base + shift
 	KEYMAP(
-		KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC, 
-		KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_ENT, 
-		_______, KC_1, KC_2, KC_3, KC_4, KC_SCLN, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, _______, KC_RSFT, 
-		KC_LSFT, KC_GRV, KC_BSLS, KC_SLSH, KC_QUOT, KC_SPC)
+		KC_MPLY,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,   _______,  _______,  _______,  _______,  _______,  KC_DEL,
+		_______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,
+		_______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,
+		_______,  _______,  _______,  _______,  _______,  _______),
 
+	//layer 1
+	KEYMAP(
+		_______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_BSPC,
+		KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_5,     KC_6,     KC_7,    KC_8,     KC_9,     KC_0,     KC_ENT,
+		_______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_SCLN,  KC_MINS,  KC_EQL,  KC_LBRC,  KC_RBRC,  _______,  KC_RSFT,
+		KC_LSFT,  KC_GRV,   KC_BSLS,  KC_SLSH,  KC_QUOT,  KC_SPC)
+	//base + lshift
+	//base + rshift
 };
 
 void matrix_init_user(void) {
