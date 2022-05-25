9ff7b97559b72b10e4906d5985c0d324876ba453
Fraser <tomatoesneverlie@gmail.com>
2022-05-25 01:05:55 -0400

    layer tweaks to improve usability
    more used keys in better positions, added second `0` and `.` on number layer for speed and comfort

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -17,17 +17,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     //layer 1 (momentary)
     KEYMAP(
-        KC_ESC,      KC_GRV,   S(KC_GRV),   S(KC_COMMA),  S(KC_DOT),  S(KC_EQL),  KC_EQL,      KC_LBRC,  KC_RBRC,  S(KC_LBRC),  S(KC_RBRC),  KC_BSPC,
-        S(KC_BSLS),  S(KC_QUOT),  KC_QUOT,  S(KC_MINS),   KC_MINS,    S(KC_5),    S(KC_6),     S(KC_7),  S(KC_8),  S(KC_9),     S(KC_0),     _______,
-        _______,     S(KC_1),     S(KC_2),  S(KC_3),      S(KC_4),    KC_COMMA,   S(KC_SCLN),  KC_SCLN,  KC_LGUI,  KC_LALT,     KC_LCTL,     _______,
-        _______,     KC_BSLS,     KC_SLSH,  S(KC_SLSH),   KC_DOT,     KC_SPC
+        KC_ESC,      KC_GRV,      S(KC_GRV),  S(KC_COMMA),  S(KC_DOT),   S(KC_5),  S(KC_6),  S(KC_7),     S(KC_8),     S(KC_9),  S(KC_0),    KC_BSPC,
+        S(KC_BSLS),  S(KC_1),     S(KC_2),    S(KC_3),      S(KC_4),     KC_SCLN,  KC_EQL,   KC_MINS,     S(KC_QUOT),  KC_QUOT,  S(KC_EQL),  _______,
+        _______,     S(KC_SLSH),  KC_DOT,     KC_COMMA,     S(KC_SCLN),  KC_LBRC,  KC_RBRC,  S(KC_MINS),  KC_LGUI,     KC_LALT,  KC_LCTL,    _______,
+        _______,     KC_BSLS,     KC_SLSH,    S(KC_LBRC),   S(KC_RBRC),  KC_SPC
     ),
     //layer 2 (momentary)
     KEYMAP(
-        KC_MPLY,  KC_MRWD,  KC_VOLD,  KC_VOLU,  KC_MFFD,  KC_F5,  KC_F6,  KC_F7,  KC_F8,    KC_F9,    KC_F10,   KC_BSPC,
-        KC_TAB,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_5,   KC_6,   KC_7,   KC_8,     KC_9,     KC_0,     _______,
-        _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_B,   KC_N,   KC_M,   KC_LGUI,  KC_LALT,  KC_LCTL,  _______,
-        _______,  KC_F11,   KC_F12,   KC_C,     KC_V,     KC_SPC
+        KC_MPLY,  KC_MRWD,  KC_VOLD,  KC_VOLU,  KC_MFFD,  KC_F5,  KC_F6,  KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_BSPC,
+        KC_TAB,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_5,   KC_6,   KC_7,    KC_8,     KC_9,     KC_0,     _______,
+        _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_B,   KC_N,   KC_DOT,  KC_LGUI,  KC_LALT,  KC_LCTL,  _______,
+        _______,  KC_F11,   KC_F12,   KC_C,     KC_0,     KC_SPC
     )
 };
 
